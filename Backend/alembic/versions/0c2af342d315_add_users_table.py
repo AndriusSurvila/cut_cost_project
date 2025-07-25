ef004a2821dfa3bcac1bdf2848d70bd7066c47e2
AndriusSurvila <andrius.survila123@gmail.com>
2025-07-25 12:59:24 +0300

    added users table

---
+++ Backend/alembic/versions/0c2af342d315_add_users_table.py
@@ -0,0 +1,46 @@
+"""add users table
+
+Revision ID: 0c2af342d315
+Revises: ba3c343568d3
+Create Date: 2025-07-25 12:50:32.325390
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision = '0c2af342d315'
+down_revision = 'ba3c343568d3'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_table('users',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('username', sa.String(), nullable=False),
+        sa.Column('email', sa.String(), nullable=False),
+        sa.Column('hashed_password', sa.String(), nullable=False),
+        sa.Column('is_active', sa.Boolean(), nullable=True),
+        sa.Column('created_at', sa.DateTime(), nullable=True),
+        sa.PrimaryKeyConstraint('id')
+    )
+    op.create_index(op.f('ix_users_email'), 'users', ['email'], unique=True)
+    op.create_index(op.f('ix_users_id'), 'users', ['id'], unique=False)
+    op.create_index(op.f('ix_users_username'), 'users', ['username'], unique=True)
+
+    with op.batch_alter_table('chats') as batch_op:
+        batch_op.add_column(sa.Column('user_id', sa.Integer(), nullable=False))
+        batch_op.create_foreign_key(None, 'users', ['user_id'], ['id'])
+
+
+def downgrade() -> None:
+    with op.batch_alter_table('chats') as batch_op:
+        batch_op.drop_constraint(None, type_='foreignkey')
+        batch_op.drop_column('user_id')
+
+    op.drop_index(op.f('ix_users_username'), table_name='users')
+    op.drop_index(op.f('ix_users_id'), table_name='users')
+    op.drop_index(op.f('ix_users_email'), table_name='users')
+    op.drop_table('users')
