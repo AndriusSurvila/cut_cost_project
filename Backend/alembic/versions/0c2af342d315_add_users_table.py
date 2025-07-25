924b3d417aa67164a5e3a63faa3dd3097740f81a
AndriusSurvila <andrius.survila123@gmail.com>
2025-07-25 13:04:00 +0300

    fix users table

--- Backend/alembic/versions/0c2af342d315_add_users_table.py
+++ Backend/alembic/versions/0c2af342d315_add_users_table.py
@@ -9,7 +9,6 @@ Create Date: 2025-07-25 12:50:32.325390
 from alembic import op
 import sqlalchemy as sa
 
-
 revision = '0c2af342d315'
 down_revision = 'ba3c343568d3'
 branch_labels = None
@@ -17,30 +16,33 @@ depends_on = None
 
 
 def upgrade() -> None:
-    op.create_table('users',
-        sa.Column('id', sa.Integer(), nullable=False),
+    op.create_table(
+        'users',
+        sa.Column('id', sa.Integer(), primary_key=True, nullable=False),
         sa.Column('username', sa.String(), nullable=False),
         sa.Column('email', sa.String(), nullable=False),
         sa.Column('hashed_password', sa.String(), nullable=False),
         sa.Column('is_active', sa.Boolean(), nullable=True),
         sa.Column('created_at', sa.DateTime(), nullable=True),
-        sa.PrimaryKeyConstraint('id')
     )
-    op.create_index(op.f('ix_users_email'), 'users', ['email'], unique=True)
     op.create_index(op.f('ix_users_id'), 'users', ['id'], unique=False)
     op.create_index(op.f('ix_users_username'), 'users', ['username'], unique=True)
+    op.create_index(op.f('ix_users_email'), 'users', ['email'], unique=True)
 
-    with op.batch_alter_table('chats') as batch_op:
-        batch_op.add_column(sa.Column('user_id', sa.Integer(), nullable=False))
-        batch_op.create_foreign_key(None, 'users', ['user_id'], ['id'])
+    op.add_column('chats', sa.Column('user_id', sa.Integer(), nullable=True))
+    op.create_foreign_key(
+        'fk_chats_user_id_users',
+        'chats', 'users',
+        ['user_id'], ['id'],
+        ondelete='CASCADE'
+    )
 
 
 def downgrade() -> None:
-    with op.batch_alter_table('chats') as batch_op:
-        batch_op.drop_constraint(None, type_='foreignkey')
-        batch_op.drop_column('user_id')
+    op.drop_constraint('fk_chats_user_id_users', 'chats', type_='foreignkey')
+    op.drop_column('chats', 'user_id')
 
+    op.drop_index(op.f('ix_users_email'), table_name='users')
     op.drop_index(op.f('ix_users_username'), table_name='users')
     op.drop_index(op.f('ix_users_id'), table_name='users')
-    op.drop_index(op.f('ix_users_email'), table_name='users')
     op.drop_table('users')
